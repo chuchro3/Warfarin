904930352d1797c4c5a1eaab6c6b792686f8cb92
Nick Guo <nick.n.guo@gmail.com>
2019-03-14 15:28:47 -0700

    added linear data loader (rows go to size 4153)

---
+++ linear_data_loader.py
@@ -0,0 +1,47 @@
+# Loads in the warfarin data file from './data/warfarin.csv'
+
+import csv
+import numpy as np
+import sys
+
+from collections import defaultdict
+from data_loader import get_data
+
+WARFARIN_FILE_PATH = './data/warfarin.csv'
+DATA_COLUMNS = 65
+LABEL_COLUMN = 'Therapeutic Dose of Warfarin'
+
+FLOAT_LABELS = ['Age', 'Height (cm)', 'Weight (kg)']
+LABELS = []
+
+# returns:
+# - data (n x m)
+# - labels (n x 1)
+# - columns_dict (size m, keyed by string from warfarin data)
+# - values_dict (size m, keyed by column name for data. each entry is a dict from data value string -> integer)
+def get_data_linear():
+    data, labels, columns_dict, values_dict = get_data()
+
+    index_labels = {}
+    for k in columns_dict:
+        index_labels[columns_dict[k]] = k
+
+    # DANGEROUS: RAN ONCE TO FIGURE OUT SHAPE.
+    NUM_ROWS = 5528
+    NUM_COLS = 4153
+    # do not change the above unless shape of data changes.
+    linearized_data = np.zeros((NUM_ROWS, NUM_COLS))
+    for i, d in enumerate(data):
+        write_index = 0
+        for j, val in enumerate(d):
+            if index_labels[j] in FLOAT_LABELS:
+                linearized_data[i,write_index] = val
+                write_index += 1
+            else:
+                assert val == int(val), 'Value must be a value index'
+                linearized_data[i,write_index + int(val)] = 1
+                write_index += len(values_dict[index_labels[j]].items())
+        assert write_index == NUM_COLS
+    return linearized_data
+
+data = get_data_linear()
