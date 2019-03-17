b0470a1e92eedb87796fb27f71f9ea00c5be7c25
Nick Guo <nick.n.guo@gmail.com>
2019-03-17 11:45:23 -0700

    updated linear data loader to be more dynamic

--- linear_data_loader.py
+++ linear_data_loader.py
@@ -7,34 +7,48 @@ import sys
 from collections import defaultdict
 from data_loader import get_data
 
-WARFARIN_FILE_PATH = './data/warfarin.csv'
-DATA_COLUMNS = 65
-LABEL_COLUMN = 'Therapeutic Dose of Warfarin'
- #FLOAT_LABELS = ['Age', 'Height (cm)', 'Weight (kg)', 'INR on Reported Therapeutic Dose of Warfarin']
-FLOAT_LABELS = ['Height (cm)', 'Weight (kg)', 'INR on Reported Therapeutic Dose of Warfarin']
-IGNORE_LABELS = ['Comorbidities', 'Medications']
+FLOAT_LABELS = [
+    'Age',
+    'Height (cm)',
+    'Weight (kg)',
+    'INR on Reported Therapeutic Dose of Warfarin',
+]
+
+IGNORE_LABELS = [
+    'Comorbidities',
+    'Medications'
+]
+
+# DO NOT ACCESS NUM_COLS BEFORE CALLING get_data_linear
+NUM_COLS = 0
+NUM_COLS_INITIALIZED = False
+
 
 # returns:
-# - data (n x m)
-# - labels (n x 1)
-# - columns_dict (size m, keyed by string from warfarin data)
-# - values_dict (size m, keyed by column name for data. each entry is a dict from data value string -> integer)
+# - linearized_data: one-hot encoding of everything except for the columns in
+#                    FLOAT_LABELS and ignoring those in IGNORE_LABELS.
+#                    data 
+# - labels: unchanged from return of get_data(); just included for convenience
 def get_data_linear():
     data, labels, columns_dict, values_dict = get_data()
 
-    NUM_COLS = 0
+    # DANGEROUS: RAN ONCE TO FIGURE OUT NUM OF ROWS.
+    NUM_ROWS = 5528
+    global NUM_COLS
+    global NUM_COLS_INITIALIZED
+    if not NUM_COLS_INITIALIZED:
+        for k in columns_dict:
+            if k in FLOAT_LABELS:
+                NUM_COLS += 1
+            elif k not in IGNORE_LABELS:
+                NUM_COLS += len(values_dict[k].items())
+        NUM_COLS_INITIALIZED = True
+    # do not change the above unless amount of data changes.
+
     index_labels = {}
     for k in columns_dict:
         index_labels[columns_dict[k]] = k
-        if k in FLOAT_LABELS:
-            NUM_COLS += 1
-        elif k not in IGNORE_LABELS:
-            NUM_COLS += len(values_dict[k].items())
-        print(k, ': ', NUM_COLS)
 
-    # DANGEROUS: RAN ONCE TO FIGURE OUT SHAPE.
-    NUM_ROWS = 5528
-    # do not change the above unless shape of data changes.
     linearized_data = np.zeros((NUM_ROWS, NUM_COLS))
     print ("SHAPE OF DATA:", linearized_data.shape)
     for i, d in enumerate(data):
@@ -49,7 +63,6 @@ def get_data_linear():
                 assert val == int(val), 'Value must be a value index'
                 linearized_data[i,write_index + int(val)] = 1
                 write_index += len(values_dict[index_labels[j]].items())
-        #print("LEN:", write_index)
         assert write_index == NUM_COLS
     return linearized_data, labels
 
