6d3340d1fc03c383a8c88c3a131f2f32523f839e
Nick Guo <nick.n.guo@gmail.com>
2019-03-18 23:24:19 -0700

    fast lasso bandit

--- lasso_bandit.py
+++ lasso_bandit.py
@@ -10,60 +10,50 @@ import datetime
 
 FEATURE_DIM = ldl.NUM_COLS
 NUM_ACTIONS = 3
-LAMBDA = 0.01
+LAMBDA = 0.1
+
+UPDATE_EVERY_X_ITERS = 1
 
 
 class LASSO_BANDIT():
-    def _compute_beta_hat(self, arm, X, Y, l=0.1):
-        # l is lambda
-        """
-        def func_beta_hat(beta):
-            return (LA.norm(Y - np.dot(X,beta))**2) / X.shape[0] + l * LA.norm(beta, 1)
+    def _predict_reward(self, arm, features):
+        return self.model[arm].predict([features])[0]
 
-        initial = np.zeros(X.shape[1])
-        return optimize.minimize(func_beta_hat, initial)
-        """
-        if (arm, X.shape) in self.mem:
-            return self.mem[(arm, X.shape)]
-        model = linear_model.Lasso(alpha=l)
-        model.fit(X, Y)
-        self.mem[(arm, X.shape)] = model.coef_
-        return model.coef_
+    # l is lambda (regularization weight)
+    def _update_model(self, arm, X, Y, l):
+        key = (arm, X.shape[0] / UPDATE_EVERY_X_ITERS)
+        if key in self.mem:
+            return # don't perform update yet
+        else:
+            #print("not in cache", key)
+            pass
 
+        self.model[arm].set_params(alpha = l)
+        self.model[arm].fit(X, Y)
+        self.mem.add(key)
 
     def __init__(self, K=NUM_ACTIONS, d=FEATURE_DIM):
-        """
-        self.alpha = alpha
-        self.A = [np.identity(self.d) for k in range(K)]
-        self.A_inv = [np.identity(self.d) for k in range(K)]
-        self.b = [np.zeros(self.d) for k in range(K)]
-        self.theta = None
-        self.regret = []
-        self.error_rate = []
-        self.cumu_regret = 0
-        self.sample_counter = 0
-
-        # evaluating A_inv and theta moved here for efficiency.
-        self.A_inv = [np.linalg.inv(a) for a in self.A]
-        self.theta = [a_inv.dot(b) for a_inv, b in zip(self.A_inv, self.b)]
-        """
         self.K = K
         self.d = d
         self.regret = []
         self.error_rate = []
         self.cumu_regret = 0
+
         # LASSO bandit specific params
-        self.T_i = [set(range(arm*200, (arm+1)*200)) for arm in range(self.K)]
+        self.T_i = [set(range(arm*50, (arm+1)*50)) for arm in range(self.K)]
         self.S_i = [[] for arm in range(self.K)]
         self.Y_i = [[] for arm in range(self.K)]
         self.timestep_t = 0
         self.lambda_0 = LAMBDA
-        # cache for compute beta hat
-        self.mem = {}
+
+        # set for what data sizes have been fit for each arm. contains no values.
+        self.mem = set()
+        # TODO: can train even faster with larger tolerance. 'tol' param for Lasso
+        self.model = [linear_model.Lasso(alpha=self.lambda_0, warm_start=True) for arm in range(self.K)]
 
 
     def __str__(self):
-        return "Linear UCB"
+        return "LASSO Bandit"
     
     def train(self, data, labels):
         for i in tqdm(range(len(labels))):
@@ -83,20 +73,27 @@ class LASSO_BANDIT():
         if action_t == -1:
             best_reward = float('-inf')
             for arm in range(self.K):
-                predicted_arm_reward = np.dot(features.T, self._compute_beta_hat(arm, np.array(self.S_i[arm]), np.array(self.Y_i[arm]), self.lambda_t))
+                predicted_arm_reward = self._predict_reward(arm, features)
                 if predicted_arm_reward > best_reward:
                     action_t = arm
                     best_reward = predicted_arm_reward
 
-        assert action_t != -1, "No arm ws selected..."
+        assert action_t != -1, "No arm was selected..."
 
         # add the arm to the observations
         self.S_i[action_t].append(features)
         # update lambda
-        self.lambda_t = self.lambda_0 * np.sqrt((np.log((self.timestep_t)) + np.log(self.d)) / self.timestep_t)
+        self.lambda_t = self.lambda_0 * np.sqrt(
+                (np.log((self.timestep_t)) + np.log(self.d)) / self.timestep_t)
 
         # play the arm, and observe the reward.
         self.Y_i[action_t].append(0 if action_t == l else -1)
+
+        # update the model for the arm that was pulled
+        self._update_model(action_t,
+                           np.array(self.S_i[action_t]),
+                           np.array(self.Y_i[action_t]),
+                           self.lambda_t)
 
         # metrics collection
         self.cumu_regret += (0 if action_t == l else -1)
@@ -127,50 +124,23 @@ class LASSO_BANDIT():
         action_t = -1
         best_reward = float('-inf')
         for arm in range(self.K):
-            predicted_arm_reward = np.dot(features.T, self._compute_beta_hat(arm, np.array(self.S_i[arm]), np.array(self.Y_i[arm]), self.lambda_t))
+            predicted_arm_reward = self._predict_reward(arm, features)
             if predicted_arm_reward > best_reward:
                 action_t = arm
                 best_reward = predicted_arm_reward
 
-        assert action_t != -1, "[eval datum] No arm ws selected..."
+        assert action_t != -1, "[eval datum] No arm was selected..."
         return action_t
 
     
-def test_lin_ucb_full(data, true_buckets, alpha=0.1):
-    lin_ucb = Lin_UCB(alpha = alpha)
-    lin_ucb.train(data, true_buckets)
-    pred_buckets = lin_ucb.evaluate(data)
-    acc = util.get_accuracy_bucketed(pred_buckets, true_buckets)
-    print("accuracy on linear UCB: " + str(acc))
-
-def plot_regret(regrets, alpha, suffix=''):
-    plt.clf()
-    plt.title('Regret - ' + str(alpha) + ' - ' + datetime.datetime.now().strftime("%D - %H:%M:%S"))
-    plt.xlabel('Samples Seen')
-    plt.ylabel('Regret')
-    plt.plot(range(1, 1+ len(regrets)), regrets)
-    if suffix == '':
-        plt.savefig('plots/regret'+str(alpha).replace('.','_')+ '_' + datetime.datetime.now().strftime('%s'))
-    else:
-        plt.savefig('plots/regret'+ '_' + suffix)
-
-def plot_error_rate(error_rates, alpha, suffix=''):
-    plt.clf()
-    plt.title('Error Rate- ' + str(alpha) + ' - ' + datetime.datetime.now().strftime("%D - %H:%M:%S"))
-    plt.xlabel('Samples Seen')
-    plt.ylabel('Cumulative Error Rate')
-    plt.plot(range(1, 1+ len(error_rates)), error_rates)
-    if suffix == '':
-        plt.savefig('plots/error'+str(alpha).replace('.','_')+ '_' +datetime.datetime.now().strftime('%s'))
-    else:
-        plt.savefig('plots/error'+ '_' + suffix)
-
+# probably do not run this for decent results (below data isn't randomized)
+# execute 'run_batches.py' instead on the lasso bandit model.
 if __name__ == '__main__':
     data, true_labels = ldl.get_data_linear()
     true_buckets = [util.bucket(t) for t in true_labels]
     
     lasso_bandit = LASSO_BANDIT()
-    lasso_bandit.train(data[:3000], true_buckets[:3000])
+    lasso_bandit.train(data[:2000], true_buckets[:2000])
     pred_buckets = lasso_bandit.evaluate(data)
     acc = util.get_accuracy_bucketed(pred_buckets, true_buckets)
     print("accuracy on LASSO bandit: " + str(acc))
